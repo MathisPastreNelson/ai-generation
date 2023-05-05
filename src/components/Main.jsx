ffbb54295086f725393a723f0f15e69cfbd3731f
MathisPastreNelson <shiray@hotmail.fr>
2023-05-05 04:53:22 +0200

    gh page and background

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import background from "../assets/videos/Quartier.mp4";
 
 export default function Main() {
-  return <div className="main">Main</div>;
+  return (
+    <div className="main">
+      <video autoPlay muted loop id="myVideo">
+        <source src={background} type="video/mp4" />
+      </video>
+
+      <div className="main_content">Test</div>
+    </div>
+  );
 }
