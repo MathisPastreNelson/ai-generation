c4dffdaa7e3a4a974063d14d45437365764c8dfa
MathisPastreNelson <shiray@hotmail.fr>
2023-05-08 14:39:35 +0200

    test reactLoading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Main from "../components/Main";
-import Loader from "../components/Loader";
+import ReactLoading from "react-loading";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
@@ -11,5 +11,18 @@ export default function Home() {
     });
   }, []);
 
-  return <div>{loading ? <Loader /> : <Main />}</div>;
+  return (
+    <div>
+      {loading ? (
+        <ReactLoading
+          type={"spin"} // Type d'indicateur de chargement
+          color={"#fff"} // Couleur de l'indicateur de chargement
+          height={100} // Hauteur de l'indicateur de chargement
+          width={100} // Largeur de l'indicateur de chargement
+        />
+      ) : (
+        <Main />
+      )}
+    </div>
+  );
 }
