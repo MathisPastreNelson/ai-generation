2edc001a9f4ce247218122fd0278c8415645e825
MathisPastreNelson <shiray@hotmail.fr>
2023-05-08 15:05:04 +0200

    retest du loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,45 @@
 import React, { useState, useEffect } from "react";
 import Main from "../components/Main";
-import ReactLoading from "react-loading";
+import Loader from "../components/Loader";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    window.addEventListener("load", () => {
+    const images = document.querySelectorAll("img");
+    const videos = document.querySelectorAll("video");
+
+    // Créer un tableau de promesses pour le chargement des images et des vidéos
+    const promises = [];
+
+    images.forEach((img) => {
+      const promise = new Promise((resolve, reject) => {
+        if (img.complete) {
+          resolve();
+        } else {
+          img.addEventListener("load", resolve);
+          img.addEventListener("error", reject);
+        }
+      });
+      promises.push(promise);
+    });
+
+    videos.forEach((video) => {
+      const promise = new Promise((resolve, reject) => {
+        if (video.readyState >= 2) {
+          resolve();
+        } else {
+          video.addEventListener("loadeddata", resolve);
+          video.addEventListener("error", reject);
+        }
+      });
+      promises.push(promise);
+    });
+
+    Promise.all(promises).then(() => {
       setLoading(false);
     });
   }, []);
 
-  return (
-    <div>
-      {loading ? (
-        <ReactLoading
-          type={"spin"} // Type d'indicateur de chargement
-          color={"#fff"} // Couleur de l'indicateur de chargement
-          height={100} // Hauteur de l'indicateur de chargement
-          width={100} // Largeur de l'indicateur de chargement
-        />
-      ) : (
-        <Main />
-      )}
-    </div>
-  );
+  return <div>{loading ? <Loader /> : <Main />}</div>;
 }
