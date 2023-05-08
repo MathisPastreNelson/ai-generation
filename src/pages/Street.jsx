7f21686192ab44a1004dbad1043a5cb6981487d3
MathisPastreNelson <shiray@hotmail.fr>
2023-05-08 15:51:15 +0200

    test loader

--- src/pages/Street.jsx
+++ src/pages/Street.jsx
@@ -10,6 +10,7 @@ export default function Street() {
     const images = document.querySelectorAll("img");
     const videos = document.querySelectorAll("video");
 
+    // Créer un tableau de promesses pour le chargement des images et des vidéos
     const promises = [];
 
     images.forEach((img) => {
@@ -45,13 +46,15 @@ export default function Street() {
     });
 
     Promise.all(promises).then(() => {
+      // Vérifier si la durée minimale est atteinte avant de mettre à jour l'état
       setTimeout(() => {
         setMinDurationReached(true);
-      }, 2000);
+      }, 1200);
     });
   }, []);
 
   useEffect(() => {
+    // Mettre à jour l'état loading si la durée minimale est atteinte
     if (minDurationReached) {
       setLoading(false);
     }
