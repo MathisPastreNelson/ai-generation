d7778e1b38840753e68e578c60a294c8d4de61af
MathisPastreNelson <shiray@hotmail.fr>
2023-05-25 02:27:20 +0200

    video on/off

--- src/components/StreetContent.jsx
+++ src/components/StreetContent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 import background from "../assets/videos/cityBackground2.mp4";
@@ -20,6 +20,20 @@ import man3 from "../assets/imagesSmall/man3.png";
 import man4 from "../assets/imagesSmall/man4.png";
 
 export default function StreetContent() {
+  const videoRefs = useRef([]);
+
+  const handleVideoToggle = (index) => {
+    const video = videoRefs.current[index];
+
+    if (video.readyState === 4) {
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    }
+  };
+
   return (
     <div className="main">
       <Header />
@@ -31,16 +45,42 @@ export default function StreetContent() {
         <h3 className="global_Container_title">Timelapse Generated</h3>
         <Slide duration="1000">
           <div className="videoStreet_Box">
-            <video className="videoStreet videoStreet1" autoPlay muted loop>
+            <video
+              className="videoStreet videoStreet1"
+              ref={(el) => (videoRefs.current[0] = el)}
+              
+              muted
+              loop
+              onClick={() => handleVideoToggle(0)}
+            >
               <source src={video1} type="video/mp4" />
             </video>
-            <video className="videoStreet videoStreet1" autoPlay muted loop>
+            <video
+              className="videoStreet videoStreet1"
+              ref={(el) => (videoRefs.current[1] = el)}
+              muted
+              loop
+              onClick={() => handleVideoToggle(1)}
+            >
               <source src={video2} type="video/mp4" />
             </video>
-            <video className="videoStreet" autoPlay muted loop>
+            <video
+              className="videoStreet"
+              ref={(el) => (videoRefs.current[2] = el)}
+              muted
+              loop
+              onClick={() => handleVideoToggle(2)}
+            >
               <source src={video3} type="video/mp4" />
             </video>
-            <video className="videoStreet videoStreet2" autoPlay muted loop>
+            <video
+              className="videoStreet videoStreet2"
+              ref={(el) => (videoRefs.current[3] = el)}
+              
+              muted
+              loop
+              onClick={() => handleVideoToggle(3)}
+            >
               <source src={video4} type="video/mp4" />
             </video>
           </div>
